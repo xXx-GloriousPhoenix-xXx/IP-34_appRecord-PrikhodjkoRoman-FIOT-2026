b0ad1007b3d3a30c7be64451c923c434cbad1c62
RomanPR <prikhodjko.roman@gmail.com>
2026-03-25 02:17:19 +0200

    save

--- base-path.js
+++ base-path.js
@@ -1,3 +1,5 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production"
+  ? "/IP-34_appRecord-PrikhodjkoRoman-FIOT-2026"
+  : "");
